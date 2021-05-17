ba1116eedf79ba1219aadcd22a4755b65e5e85d2
Trevor McKay <trmckay@pm.me>
2021-05-17 00:16:01 -0700

    update doc and publish pipeline

--- doc/publish.sh
+++ doc/publish.sh
@@ -7,10 +7,11 @@ echo Updating using Neuron Docker.
 echo Close when done.
 
 cd "$GIT_ROOT/doc" || exit 1
+rm -rf .neuron
 docker-compose up
 
 echo "Publishing changes to gh-pages branch."
 cd "$GIT_ROOT" || exit 1
-git subtree push --prefix "$GIT_ROOT/doc/markdown/.neuron/output" origin gh-pages
+git subtree push --prefix "$GIT_ROOT/doc/.neuron/output" origin gh-pages
 
 cd "$PWD" || exit 1
