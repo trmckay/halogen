7408e273b40216670ad264760621e62354a37281
Trevor McKay <trmckay@pm.me>
2021-05-19 10:36:40 -0700

    remove Otter-specific config/code, prep for rv64gc

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,19 +1,11 @@
 #![no_std]
 #![no_main]
 
-mod mmio;
 mod panic;
-
-use core::ptr::*;
-use mmio::*;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {
-        unsafe {
-            let v = read_volatile(mmio::SWITCHES);
-            write_volatile(mmio::LEDS, v);
-            write_volatile(mmio::SSEG, 0xFFFF - v);
-        }
-    }
+    let str = b"Hello, world.";
+
+    loop {}
 }
--- kernel/src/mmio.rs
+++
@@ -1,3 +0,0 @@
-pub const LEDS: *mut u16 = (0x11080000) as *mut u16;
-pub const SSEG: *mut u16 = (0x110C0000) as *mut u16;
-pub const SWITCHES: *const u16 = (0x11000000) as *const u16;
