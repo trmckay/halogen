1077cb9fee1d074593884da53167836ee185be75
Trevor McKay <trmckay@pm.me>
2021-05-20 08:42:25 -0700

    rustfmt

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -12,24 +12,24 @@ pub mod uart;
 #[macro_export]
 macro_rules! print
 {
-	($($args:tt)+) => ({
-			use core::fmt::Write;
-			let _ = write!(crate::uart::Uart::new(uart::UART_MMIO_ADDR), $($args)+);
-	});
+    ($($args:tt)+) => ({
+        use core::fmt::Write;
+        let _ = write!(crate::uart::Uart::new(uart::UART_MMIO_ADDR), $($args)+);
+    });
 }
 
 #[macro_export]
 macro_rules! println
 {
-	() => ({
-		print!("\n")
-	});
-	($fmt:expr) => ({
-		print!(concat!($fmt, "\n"))
-	});
-	($fmt:expr, $($args:tt)+) => ({
-		print!(concat!($fmt, "\n"), $($args)+)
-	});
+    () => ({
+        print!("\n")
+    });
+    ($fmt:expr) => ({
+        print!(concat!($fmt, "\n"))
+    });
+    ($fmt:expr, $($args:tt)+) => ({
+        print!(concat!($fmt, "\n"), $($args)+)
+    });
 }
 
 #[no_mangle]
