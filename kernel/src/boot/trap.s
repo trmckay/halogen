ecb13684c5a244948c3e90b72b7ee1908c26c9f2
Trevor McKay <tm@trmckay.com>
2021-07-04 20:17:00 -0700

    simplify high-level organization

--- kernel/src/boot/trap.s
+++
@@ -1,8 +0,0 @@
-.section .text
-
-.global mtrap_vector
-
-# Kernel trap vector.
-# Handles syscalls and exceptions.
-mtrap_vector:
-    mret
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,19 +1,52 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message, global_asm, asm, exclusive_range_pattern)]
+#![allow(dead_code)]
 
 global_asm!(include_str!("boot/boot.s"));
-global_asm!(include_str!("boot/trap.s"));
 
 pub mod driver;
 pub mod memory;
 pub mod process;
 
 mod panic;
-mod print;
 
 #[no_mangle]
 pub extern "C" fn kernel() -> ! {
     println!("Hello, world.");
     panic!();
+}
+
+#[no_mangle]
+pub extern "C" fn mtrap_vector() {
+    unsafe {
+        asm!("mret");
+    }
+}
+
+#[macro_export]
+macro_rules! print
+{
+    ($($args:tt)+) => ({
+        // Use the platform-specific UART driver.
+        use crate::driver::Uart;
+        use core::fmt::Write;
+
+        let mut uart = Uart::new();
+        let _ = write!(uart, $($args)+);
+    });
+}
+
+#[macro_export]
+macro_rules! println
+{
+    () => ({
+        print!("\n")
+    });
+    ($fmt:expr) => ({
+        print!(concat!($fmt, "\n"))
+    });
+    ($fmt:expr, $($args:tt)+) => ({
+        print!(concat!($fmt, "\n"), $($args)+)
+    });
 }
