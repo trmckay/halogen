0cb163f4e68813f7a8790fa0e22278629b8bef98
Trevor McKay <tm@trmckay.com>
2021-09-08 18:15:02 -0700

    dump correct region of kernel memory

--- lab-os/src/panic.rs
+++ lab-os/src/panic.rs
@@ -3,34 +3,34 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    let pc: usize;
+    let ra: usize;
     let sp: usize;
     let gp: usize;
     let fp: usize;
 
     // Save some special registers.
     unsafe {
-        asm!("mv {}, ra", out(reg) pc);
+        asm!("mv {}, ra", out(reg) ra);
         asm!("mv {}, sp", out(reg) sp);
         asm!("mv {}, fp", out(reg) fp);
         asm!("mv {}, gp", out(reg) gp);
     }
 
-    // Print debug info.
     println!("Kernel panic!\n");
 
-    println!("ra = 0x{:08X}", pc);
+    println!("ra = 0x{:08X}", ra);
     println!("sp = 0x{:08X}", sp);
     println!("fp = 0x{:08X}", fp);
     println!("gp = 0x{:08X}\n", gp);
 
-    // Dump the stack.
-    let stack_end: usize;
+    let text: usize;
+    let text_end: usize;
     unsafe {
-        asm!("la {}, _stack_end", out(reg) stack_end);
+        asm!("la {}, _MEM", out(reg) text);
+        asm!("la {}, _K_STACK", out(reg) text_end);
     }
-    println!("Stack dump:\n");
-    print_dump(sp, stack_end - sp);
+    println!("Text and data dump:\n");
+    print_dump(text, text_end - text);
 
     // Restart
     loop {}
